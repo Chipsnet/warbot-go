d73b54dca3dd533fc789a691dc63d15f151c478a
Minato86 <doronomori660@outlook.jp>
2020-09-20 01:04:34 +0900

    lint無効

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/chipsnet/warbot-go
 
 go 1.14
 
-require github.com/go-sharp/color v1.9.1
+require (
+	github.com/bwmarrin/discordgo v0.22.0
+	github.com/go-sharp/color v1.9.1
+)
--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"github.com/bwmarrin/discordgo"
 	"github.com/go-sharp/color"
+	"log"
 )
 
 // Version バージョン情報
 const Version = "1.0.0"
 
 func main() {
+	log.Print("hello")
+
 	color.Yellow(` _       _____    ____  ____        __             
 | |     / /   |  / __ \/ __ )____  / /_ ____ _____ 
 | | /| / / /| | / /_/ / __  / __ \/ __// __ / __ \
